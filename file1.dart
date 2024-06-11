65a57aa36d7778d9f15e5227e458e327d3c4b851
tran bao toan <tbtoanit@gmail.com>
2024-06-11 19:22:49 +0700

    modify something

--- file1.dart
+++ file1.dart
@@ -2,21 +2,13 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  // Yêu cầu người dùng nhập bán kính
   stdout.write('Nhập bán kính của hình tròn: ');
   String? input = stdin.readLineSync();
-
-  // Chuyển đổi giá trị nhập vào từ chuỗi sang số
   double? radius = double.tryParse(input!);
-
-  // Kiểm tra nếu giá trị nhập vào là hợp lệ
   if (radius == null) {
     print('Giá trị nhập vào không hợp lệ.');
   } else {
-    // Tính toán diện tích của hình tròn
     double area = pi * radius * radius;
-
-    // In ra diện tích
     print('Diện tích của hình tròn có bán kính $radius là: $area');
   }
 }
